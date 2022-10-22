6104d68487781d478bf1e85c90774fe12d52b3ae
Christian <christiand0393@gmail.com>
2022-10-21 22:16:58 -0500

    arreglo 3

--- view/plantilla.php
+++ view/plantilla.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="view/static/inicio.css">
+    <link rel="stylesheet" href="view/static/nicepage.css">
+    <script src="view/static/nicepage.js"></script>
+    <script src="view/static/jquery-1.9.1.min.js"></script>
 </head>
 <body>
-    
+<?php include_once "paginas/inicio.php";?>
 </body>
 </html>
