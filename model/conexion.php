9a68666f31e0d55deffa46b672236038290b377a
Christian <christiand0393@gmail.com>
2022-10-23 12:01:43 -0500

    conexion

---
+++ model/conexion.php
@@ -0,0 +1,51 @@
+<?php
+
+	class conexion
+	{
+
+		public function conectar()
+		{
+
+			
+			$server="192.168.101.4";
+			$conexion=array("Database"=>"Euler",
+			"UID"=>"sa",
+			"PWD"=>"123456",
+			"CharacterSet"=>"UTF-8");
+			
+			// $server="192.168.0.231";
+			// $conexion=array("Database"=>"Argosal",
+			// 				"UID"=>"paraclase",
+			// 				"PWD"=>"12345",
+			// 				"CharacterSet"=>"UTF-8");
+
+			$con=sqlsrv_connect($server,$conexion);
+
+			if ($con) 
+			{
+
+				return $con;
+
+			}
+			else
+			{
+				echo "Fallo en la conexion con el primer servidor...";
+				die( print_r( sqlsrv_errors(), true));
+
+			}
+			
+
+		}
+
+		public function desconectar()
+		{
+
+			$con=null;
+			return $con;
+
+		}
+
+	}
+
+
+?>
