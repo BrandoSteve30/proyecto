fe3185df1361e1e5ff8fd2594233111adb90bb4e
Christian <christiand0393@gmail.com>
2022-11-01 16:15:08 -0500

    estructura

--- view/plantilla.php
+++ view/plantilla.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="view/static/inicio.css">
-    <link rel="stylesheet" href="view/static/nicepage.css">
-    <script src="view/static/nicepage.js"></script>
-    <script src="view/static/jquery-1.9.1.min.js"></script>
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio</title>
+  <link rel="icon" href="view/static/img/logo_leonard.png">
+
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/8e504b8187.js" crossorigin="anonymous"></script>
+  
+  <link rel="stylesheet" href="view/static/style.css">
 </head>
 <body>
 <?php include_once "paginas/inicio.php";?>
